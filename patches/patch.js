a6d0d19e655cda6b26bd4668cc1ccddcaf595184
Moshe Kolodny <mkolodny@google.com>
2018-03-02 14:06:20 -0500

    Fixed broken mutate-rows tests on GAPIC branch nextickv2

--- patches/patch.js
+++ patches/patch.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const glob = require('glob');
 const path = require('path');
 
-const patched = `${__dirname}/patched-process-nextick-args.js`;
+const patchedV1 = `${__dirname}/patched-process-nextick-args-v1.js`;
+const patchedV2 = `${__dirname}/patched-process-nextick-args-v2.js`;
 glob.sync('./**/process-nextick-args').forEach(pnaDirectoryPath => {
   const packageJson = require(path.join('../', pnaDirectoryPath, 'package.json'));
   const majorVersion = parseInt(packageJson.version[0], 10);
 
   if (majorVersion <= 1) {
-    fs.copyFileSync(patched, path.join(pnaDirectoryPath, 'index.js'));
+    fs.copyFileSync(patchedV1, path.join(pnaDirectoryPath, 'index.js'));
+  } else {
+    fs.copyFileSync(patchedV2, path.join(pnaDirectoryPath, 'index.js'));
   }
 });
