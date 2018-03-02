a6a3c8c5bc2b79b02d1ba24d5ab2b6024609341f
Stephen Sawchuk <stephenplusplusplus@gmail.com>
2018-03-02 11:04:16 -0500

    Remove patches

--- patches/patch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const glob = require('glob');
-
-const patched = `${__dirname}/patched-process-nextick-args.js`;
-glob.sync('./**/process-nextick-args/index.js').forEach(file => {
-  fs.copyFileSync(patched, file);
-});
